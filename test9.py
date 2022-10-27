f09a31c5e27a63c1070a0ca7ab5016ac1636efb3
chenfeng <375541882@qq.com>
2022-10-27 17:41:41 +0800

    能运行版本

---
+++ test9.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: anchen
+# @Date:   2022-09-30 13:06:26
+# @Last Modified by:   anchen
+# @Last Modified time: 2022-10-20 13:55:46
+#  610A0000  6165EC6E  5BEC6E
+
+from __future__ import print_function
+import frida
+import sys
+
+
+def on_message(message, data):
+    if message['type'] == 'send':
+        print(message)
+        # print(message['payload']['resource'])
+        print(message['payload']['xml'])
+        # print(data)
+        # base = message['payload']['wxid']
+        # size = int(message['payload']['text'])
+        # print(hex(base), size)
+    elif message['type'] == 'error':
+        print(message)
+        # for i in message:
+        #     if i == "type":
+        #         print("[*] %s" % "error:")
+        #         continue
+        #     if type(message[i]) is str:
+        #         print("[*] %s" %
+        #               i + ":\n    {0}".format(message[i].replace('\t', '    ')))
+        #     else:
+        #         print("[*] %s" %
+        #               i + ":\n    {0}".format(message[i]))
+    else:
+        print(message)
+
+
+# F为公众号消息
+def main(target_process):
+    session = frida.attach(target_process)
+    print("公众号")
+    script = session.create_script("""
+        var ModAddress = Process.findModuleByName('WeChatWin.dll');
+        var hookAddress = ModAddress.base.add('0x759654');
+        Interceptor.attach(hookAddress , {
+            onEnter:function(args) {
+                var esi = this.context.esi;
+                var emu = Memory.readPointer(esi);
+                var leixing = Memory.readInt(emu.add('0x4C'));
+                if(leixing == 15 ){
+                    var leixing = '公众号';
+                    var xmlPro =  emu.add('0x94');
+                    var xml = Memory.readUtf16String(Memory.readPointer(xmlPro));
+                    send({'xml':xml,'leixing':leixing});
+                }else if(leixing == 20){
+                    var leixing = '群消息';
+                    var xml = Memory.readUtf16String(Memory.readPointer(emu.add('0x70')));
+                    send({'xml':xml,'leixing':leixing});
+                }else if(leixing == 19){
+                    var leixing = '个人消息';
+                    var xml = Memory.readUtf16String(Memory.readPointer(emu.add('0x70')));
+                    send({'xml':xml,'leixing':leixing});
+                }else{
+                     var xml = Memory.readUtf16String(Memory.readPointer(emu.add('0x70')));
+                     send({'xml':xml,'leixing':leixing});
+                }
+                
+            }
+        } );
+        """)
+    script.on('message', on_message)
+    script.load()
+    print("[!] Ctrl+D on UNIX, Ctrl+Z on Windows/cmd.exe to detach from instrumented program.\n\n")
+    sys.stdin.read()
+    session.detach()
+
+
+if __name__ == '__main__':
+    main('WeChat.exe')
